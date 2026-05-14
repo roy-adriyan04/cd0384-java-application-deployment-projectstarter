6a06c387f1cdbb86b8652482f8c49d41829f9e38
Roy-Adriyan <ar7818@srmist.edu.in>
2026-05-14 20:45:13 +0530

    update module-info.java

--- starter/catpoint-parent/security-service/src/main/java/module-info.java
+++ starter/catpoint-parent/security-service/src/main/java/module-info.java
@@ -3,9 +3,8 @@ module security.service {
     requires java.desktop;
     requires java.prefs;
     requires com.google.common;
+    requires com.google.gson;
+    requires com.miglayout.swing;
     requires org.slf4j;
-    exports com.udacity.catpoint.application;
-    exports com.udacity.catpoint.service;
-    exports com.udacity.catpoint.data;
-    opens com.udacity.catpoint.data;
+    opens com.udacity.catpoint.data to com.google.gson;
 }
